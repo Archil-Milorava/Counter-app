d003591423a0c806885db9f8447cbe8937a8dff8
Achi Milorava <achimilorava16@gmail.com>
2023-07-18 13:13:09 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const btns = document.querySelectorAll(".btn");
+const value = document.querySelector(".numberCount");
+let count = 0;
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    value.textContent = count;
+  });
+});
